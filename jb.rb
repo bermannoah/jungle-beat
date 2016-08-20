b191eb69445d80d3b14e42ceed2d48f1a0752e78
Noah Berman <noah@noahberman.org>
2016-08-20 16:33:52 -0600

    initial commit, iteration 0

---
+++ jb.rb
@@ -0,0 +1,8 @@
+require "./node.rb"
+require "pry"
+
+node = Node.new("plop")
+
+node.data
+node.next_node
+binding.pry
---
+++ node.rb
@@ -0,0 +1,17 @@
+class Node
+  attr_accessor :node, :next_node
+
+  def initialize(node)
+    @node = node
+  end
+
+  def data
+    node
+  end
+
+  def next_node
+    next_node = nil
+    next_node
+  end
+
+end
